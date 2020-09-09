a279f40a08d95a7ed54038f307c24e2d3e22ca7b
unknown <viniciusbeloti7@gmail.com>
2020-09-09 16:48:05 -0300

    refactor: change trigger

--- src/config/database.ts
+++ src/config/database.ts
@@ -19,11 +19,11 @@ db.once('open', () => {
 
         if (change.operationType === 'insert') {
             const messageDetails = change.fullDocument;
-            console.log(messageDetails);
             pusher.trigger('messages', 'inserted', {
                 name: messageDetails.name,
                 message: messageDetails.message,
-                timestamp: messageDetails.timestamp
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received
             });
 
         } else {
